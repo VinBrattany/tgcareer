40fd3da8c5dd04e802176a05fce65edf06f8814e
张伟[178446] <1009882887@qq.com>
2019-03-31 22:48:37 +0800

    add

--- src/main/java/com/main/tgcareer/modules/user/controller/UserController.java
+++ src/main/java/com/main/tgcareer/modules/user/controller/UserController.java
@@ -7,6 +7,8 @@ import com.main.tgcareer.common.web.BaseController;
 import com.main.tgcareer.modules.user.entity.User;
 import com.main.tgcareer.modules.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
@@ -14,12 +16,16 @@ import java.util.LinkedHashMap;
 
 @RestController
 @RequestMapping("user")
+@PropertySource(value = "classpath:city.properties",ignoreResourceNotFound = true,encoding = "UTF-8" )
 public class UserController extends BaseController{
     @Autowired
     private UserService userService;
 
     @Autowired
     private RedisServiceImpl redisService;
+
+    @Value("${city}")
+    String city;
     /**
      * 获取用户信息
      */
@@ -56,6 +62,11 @@ public class UserController extends BaseController{
         LinkedHashMap<String,Object> map = new LinkedHashMap<>();
         map.put("data",newUser);
         return Ajax.success(map);
+    }
+    @RequestMapping(method = RequestMethod.GET,value = "city",produces = {"application/json;charset=UTF-8"})
+    public String city(){
+
+        return city;
     }
     /**
      * 删除用户信息
