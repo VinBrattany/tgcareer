75debf1cea472f248e334076438d90cad14d0bbd
wangfeizhou <18819466397@163.com>
2019-03-31 16:37:05 +0800

    add

--- sql+/vinbrattany.sql
+++ sql+/vinbrattany.sql
@@ -22,31 +22,31 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `candidate`;
 CREATE TABLE `candidate` (
-  `id` varchar(32) COLLATE utf8mb4_cs_0900_ai_ci NOT NULL,
-  `name` varchar(12) COLLATE utf8mb4_cs_0900_ai_ci NOT NULL,
-  `phone` varchar(11) COLLATE utf8mb4_cs_0900_ai_ci NOT NULL,
+  `id` varchar(32) COLLATE utf8mb4_general_ci NOT NULL,
+  `name` varchar(12) COLLATE utf8mb4_general_ci NOT NULL,
+  `phone` varchar(11) COLLATE utf8mb4_general_ci NOT NULL,
   `sex` tinyint(1) NOT NULL,
   `age` tinyint(1) NOT NULL,
   `annual_salary` decimal(7,2) DEFAULT NULL,
   `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_cs_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- ----------------------------
 -- Table structure for salary
 -- ----------------------------
 DROP TABLE IF EXISTS `salary`;
 CREATE TABLE `salary` (
-  `id` varchar(32) COLLATE utf8mb4_cs_0900_ai_ci NOT NULL,
-  `city` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_cs_0900_ai_ci NOT NULL,
-  `job` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_cs_0900_ai_ci NOT NULL,
-  `corporation` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_cs_0900_ai_ci NOT NULL,
-  `contact` varchar(255) COLLATE utf8mb4_cs_0900_ai_ci NOT NULL,
+  `id` varchar(32) COLLATE utf8mb4_general_ci NOT NULL,
+  `city` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `job` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `corporation` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `contact` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
   `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_cs_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- ----------------------------
 -- Table structure for user
@@ -55,23 +55,23 @@ DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
   `id` varchar(32) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `name` varchar(12) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-  `city` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_cs_0900_ai_ci NOT NULL,
+  `city` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `sex` tinyint(1) NOT NULL,
   `age` tinyint(1) unsigned NOT NULL,
-  `phone` varchar(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_cs_0900_ai_ci DEFAULT NULL,
-  `college` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_cs_0900_ai_ci DEFAULT NULL,
-  `eduction` varchar(20) COLLATE utf8mb4_cs_0900_ai_ci DEFAULT NULL,
+  `phone` varchar(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `college` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `eduction` varchar(20) COLLATE utf8mb4_general_ci DEFAULT NULL,
   `push` tinyint(1) DEFAULT NULL,
   `expected_annual_salary` decimal(7,2) unsigned DEFAULT NULL,
   `annual_salary` decimal(7,2) DEFAULT NULL,
   `monthly_salary` decimal(7,2) DEFAULT NULL,
-  `open_id` varchar(28) CHARACTER SET utf8mb4 COLLATE utf8mb4_cs_0900_ai_ci NOT NULL,
-  `pet_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `photo_path` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_cs_0900_ai_ci DEFAULT NULL,
+  `open_id` varchar(28) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `pet_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `photo_path` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE KEY `openid` (`open_id`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_cs_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 SET FOREIGN_KEY_CHECKS = 1;
